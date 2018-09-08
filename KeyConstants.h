4f53d8a746b6a7c7d290924f0859ee5547a88acc
Nikita Pyanykh <nickkomarowski@gmail.com>
2018-09-08 18:45:38 +0300

    Added Helper header

--- KeyConstants.h
+++ KeyConstants.h
@@ -20,7 +20,7 @@ class Keys
         static std::map<int, KeyPair> KEYS;
 };
 
-map<int, KeyPair> Keys::KEYS = {
+std::map<int, KeyPair> Keys::KEYS = {
     {0xC1, {"[VK_ABNT_C1]", "[Abnt C1]"}},
     {0xC2, {"[VK_ABNT_C2]", "[Abnt C2]"}},
     {0x6B, {"[VK_ADD]", "[Numpad +]"}},
